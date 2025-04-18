89789e9357426cf57d521faa35eb6face53eba4f
yanyan0315 <k024g0064@g.neec.ac.jp>
2025-04-18 10:35:21 +0900

    3次元回転行列を作成した

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <Novice.h>
-#include <math.h>
+#include <cmath>
 
 const char kWindowTitle[] = "LE2B_25_ミヤザワハルヒ_MT3";
 
@@ -11,6 +11,12 @@ static const int kClownWidth = 60;
 typedef struct Matrix4x4 {
 	float m[4][4];
 }Matrix4x4;
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
 
 //1.行列の加法
 Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
@@ -187,6 +193,39 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& m, const char* label) {
 	}
 }
 
+//==================================================
+// 確認課題00-04
+//==================================================
+
+//1.X軸回転行列
+Matrix4x4 MakeRotationXMatrix(float radian) {
+	Matrix4x4 result = MakeIdentity4x4();
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
+	result.m[2][1] = -std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	return result;
+}
+
+//2.Y軸回転行列
+Matrix4x4 MakeRotationYMatrix(float radian) {
+	Matrix4x4 result = MakeIdentity4x4();
+	result.m[0][0] = std::cos(radian);
+	result.m[0][2] = -std::sin(radian);
+	result.m[2][0] = std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	return result;
+}
+//3.Z軸回転行列
+Matrix4x4 MakeRotationZMatrix(float radian) {
+	Matrix4x4 result = MakeIdentity4x4();
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[1][0] = -std::sin(radian);
+	result.m[1][1] = std::cos(radian);
+	return result;
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -197,16 +236,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-	Matrix4x4 m1 = { 3.2f,0.7f,9.6f,4.4f,
-					 5.5f,1.3f,7.8f,2.1f,
-					 6.9f,8.0f,2.6f,1.0f,
-					 0.5f,7.2f,5.1f,3.3f };
-
-
-	Matrix4x4 m2 = { 4.1f,6.5f,3.3f,2.2f,
-					 8.8f,0.6f,9.9f,7.7f,
-					 1.1f,5.5f,6.6f,0.0f,
-					 3.3f,9.9f,8.8f,2.2f };
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Matrix4x4 rotateXMatrix = MakeRotationXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotationYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotationZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix,rotateZMatrix));
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -221,15 +255,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Matrix4x4 resultAdd = Add(m1, m2);
-		Matrix4x4 resultMultiply = Multiply(m1, m2);
-		Matrix4x4 resultSubtract = Subtract(m1, m2);
-		Matrix4x4 inverseM1 = inverse(m1);
-		Matrix4x4 inverseM2 = inverse(m2);
-		Matrix4x4 transposeM1 = Transpose(m1);
-		Matrix4x4 transposeM2 = Transpose(m2);
-		Matrix4x4 identity = MakeIdentity4x4();
-
+		
 		///
 		/// ↑更新処理ここまで
 		///
@@ -238,21 +264,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		MatrixScreenPrintf(0, 0, resultAdd, "Add");
+		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
+		
+		MatrixScreenPrintf(0, kRowHeight*5, rotateYMatrix, "rotateYMatrix");
 
-		MatrixScreenPrintf(0, kRowHeight * 5, resultSubtract, "Subtract");
+		MatrixScreenPrintf(0, kRowHeight * 5*2, rotateZMatrix, "rotateZMatrix");
 
-		MatrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultiply, "Multiply");
+		MatrixScreenPrintf(0, kRowHeight * 5*3, rotateXYZMatrix, "rotateXYZMatrix");
 
-		MatrixScreenPrintf(0, kRowHeight * 5 * 3, inverseM1, "inverseM1");
-
-		MatrixScreenPrintf(0, kRowHeight * 5 * 4, inverseM2, "inverseM2");
-
-		MatrixScreenPrintf(kClownWidth * 5, kRowHeight * 5, transposeM1, "transposeM1");
-
-		MatrixScreenPrintf(kClownWidth * 5, kRowHeight * 5 * 2, transposeM2, "transposeM2");
-
-		MatrixScreenPrintf(kClownWidth * 5, kRowHeight * 5 * 3, identity, "identity");
 
 		///
 		/// ↑描画処理ここまで
